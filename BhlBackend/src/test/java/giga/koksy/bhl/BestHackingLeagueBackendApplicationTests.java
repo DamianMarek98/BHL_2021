b2d2c637a69ddb002201325da09cd65ffdec1e31
Deny <zmdddamian@gmail.com>
2021-04-07 20:44:16 +0200

    backend initial commit

---
+++ BhlBackend/src/test/java/giga/koksy/bhl/BestHackingLeagueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package giga.koksy.bhl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestHackingLeagueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
