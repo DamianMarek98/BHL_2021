a6b95c4b4ef0bb1d6b66451e0ee01258f47ac209
Deny <zmdddamian@gmail.com>
2021-04-10 12:37:24 +0200

    cors api fix

--- api/weather.py
+++ api/weather.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 import requests
 
@@ -10,6 +11,15 @@ import helper
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Weather(BaseModel):
     time: str = None
